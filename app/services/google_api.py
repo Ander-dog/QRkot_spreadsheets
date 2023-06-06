1d0dbd88f394e402f315bd4465149629a754035e
Андрей Алексеев <0306andrey@gmail.com>
2023-06-06 23:59:11 +0300

    Новая попытка

--- app/services/google_api.py
+++ app/services/google_api.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import List
+from typing import List, Dict, Union, Any
 
 from aiogoogle import Aiogoogle
 
@@ -9,11 +9,13 @@ from app.models import CharityProject
 FORMAT = "%Y/%m/%d %H:%M:%S"
 
 
-async def spreadsheets_create(wrapper_services: Aiogoogle) -> str:
-    now_date_time = datetime.now().strftime(FORMAT)
-    service = await wrapper_services.discover('sheets', 'v4')
+def generate_spreadsheet_config(
+    time: datetime = None
+) -> Dict[str, Union[Dict[str, str], List[Dict]]]:
+    if time is None:
+        time = datetime.now().strftime(FORMAT)
     spreadsheet_body = {
-        'properties': {'title': f'Отчет по инвестициям на {now_date_time}',
+        'properties': {'title': f'Отчет по инвестициям на {time}',
                        'locale': 'ru_RU'},
         'sheets': [{'properties': {'sheetType': 'GRID',
                                    'sheetId': 0,
@@ -21,41 +23,71 @@ async def spreadsheets_create(wrapper_services: Aiogoogle) -> str:
                                    'gridProperties': {'rowCount': 100,
                                                       'columnCount': 3}}}]
     }
+    return spreadsheet_body
+
+
+def generate_table_header(
+    time: datetime = None
+) -> List[List[Union[str, datetime]]]:
+    if time is None:
+        time = datetime.now().strftime(FORMAT)
+    header_table_values = [
+        ['Отчет от', time],
+        ['Топ проектов по скорости закрытия'],
+        ['Название проекта', 'Время сбора', 'Описание'],
+    ]
+    return header_table_values
+
+
+def generate_permissions_body(email: str) -> Dict[str, str]:
+    permissions_body = {
+        'type': 'user',
+        'role': 'writer',
+        'emailAddress': email
+    }
+    return permissions_body
+
+
+def generate_request_body(
+        values: List[List[Any]]
+    ) -> Dict[str, Union[str, List[List[Any]]]]:
+    request_body = {
+        'majorDimension': 'ROWS',
+        'values': values
+    }
+    return request_body
+
+async def spreadsheets_create(wrapper_services: Aiogoogle) -> str:
+    service = await wrapper_services.discover('sheets', 'v4')
+    spreadsheet_body = generate_spreadsheet_config()
     response = await wrapper_services.as_service_account(
         service.spreadsheets.create(json=spreadsheet_body)
     )
-    spreadsheetid = response['spreadsheetId']
-    return spreadsheetid
+    spreadsheet_id = response['spreadsheetId']
+    return spreadsheet_id
 
 
 async def set_user_permissions(
-        spreadsheetid: str,
+        spreadsheet_id: str,
         wrapper_services: Aiogoogle
 ) -> None:
-    permissions_body = {'type': 'user',
-                        'role': 'writer',
-                        'emailAddress': settings.email}
+    permissions_body = generate_permissions_body(settings.email)
     service = await wrapper_services.discover('drive', 'v3')
     await wrapper_services.as_service_account(
         service.permissions.create(
-            fileId=spreadsheetid,
+            fileId=spreadsheet_id,
             json=permissions_body,
-            fields="id"
+            fields='id'
         ))
 
 
 async def spreadsheets_update_value(
-        spreadsheetid: str,
+        spreadsheet_id: str,
         charity_projects: List[CharityProject],
         wrapper_services: Aiogoogle
 ) -> None:
-    now_date_time = datetime.now().strftime(FORMAT)
     service = await wrapper_services.discover('sheets', 'v4')
-    table_values = [
-        ['Отчет от', now_date_time],
-        ['Топ проектов по скорости закрытия'],
-        ['Название проекта', 'Время сбора', 'Описание'],
-    ]
+    table_values = generate_table_header()
     for project in charity_projects:
         new_row = [
             project.name,
@@ -64,16 +96,14 @@ async def spreadsheets_update_value(
         ]
         table_values.append(new_row)
 
-    update_body = {
-        'majorDimension': 'ROWS',
-        'values': table_values
-    }
-    sheet_range = 'A1:C' + str(len(charity_projects) + 3)
+    append_body = generate_request_body(table_values)
+    sheet_range = 'A1:C' + str(len(table_values))
     await wrapper_services.as_service_account(
-        service.spreadsheets.values.update(
-            spreadsheetId=spreadsheetid,
+        service.spreadsheets.values.append(
+            spreadsheetId=spreadsheet_id,
             range=sheet_range,
             valueInputOption='USER_ENTERED',
-            json=update_body
+            insertDataOption='OVERWRITE',
+            json=append_body
         )
     )
